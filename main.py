5a2e77a06698b4c82a74cff52b0637ab84950b03
constantinserediuc <serediuc.constantin@info.uaic.ro>
2018-12-29 17:05:51 +0200

    wip

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from keras.layers.pooling import MaxPooling2D
 from keras.utils import to_categorical
 
 if __name__ == '__main__':
-    # (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+    (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
     model = Sequential()
 
     model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
@@ -33,21 +33,20 @@ if __name__ == '__main__':
     # model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
-    # Compile the model
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer=Adadelta(),
-    #               metrics=['accuracy'])
-    #
-    # # # Train the model
-    # model.fit(X_train / 255.0, to_categorical(Y_train),
-    #           batch_size=128,
-    #           shuffle=True,
-    #           epochs=250,
-    #           validation_data=(X_test / 255.0, to_categorical(Y_test)),
-    #           callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
-    #
-    # # Evaluate the model
-    # scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adadelta(),
+                  metrics=['accuracy'])
+
+    # # Train the model
+    model.fit(X_train / 255.0, to_categorical(Y_train),
+              batch_size=128,
+              shuffle=True,
+              epochs=250,
+              validation_data=(X_test / 255.0, to_categorical(Y_test)),
+              callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
+
+    # Evaluate the model
+    scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
     print(model.summary())
-    # print('Loss: %.3f' % scores[0])
-    # print('Accuracy: %.3f' % scores[1])
+    print('Loss: %.3f' % scores[0])
+    print('Accuracy: %.3f' % scores[1])
