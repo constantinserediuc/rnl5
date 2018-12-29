4723b2d1363b35ee776e6e4bb6f77c7504c09245
constantinserediuc <serediuc.constantin@info.uaic.ro>
2018-12-29 17:51:43 +0200

    wip

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D
-from keras.optimizers import Adadelta
+from keras.optimizers import Adadelta, Adam
 from keras.layers.pooling import MaxPooling2D
 from keras.utils import to_categorical
 
@@ -13,52 +13,54 @@ if __name__ == '__main__':
 
     model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
     model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
+
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',  padding='same'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',  padding='same'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
 
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
-
     model.compile(loss='categorical_crossentropy',
-                  optimizer=Adadelta(),
+                  optimizer=Adam(),
                   metrics=['accuracy'])
 
-    #
+    # # Train the model
     model.fit(X_train / 255.0, to_categorical(Y_train),
               batch_size=128,
               shuffle=True,
               epochs=250,
               validation_data=(X_test / 255.0, to_categorical(Y_test)),
-              callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
+              callbacks=[EarlyStopping(min_delta=0.0001, patience=3)])
 
+    # Evaluate the model
     scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
     print(model.summary())
     print('Loss: %.3f' % scores[0])
