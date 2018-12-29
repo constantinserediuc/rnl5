883f5a74c342bb168880750ea76175b309ba8426
constantinserediuc <serediuc.constantin@info.uaic.ro>
2018-12-29 17:22:26 +0200

    wip

--- main.py
+++ main.py
@@ -12,32 +12,46 @@ if __name__ == '__main__':
     model = Sequential()
 
     model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
+
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
-    # model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
-    # model.add(Dense(256, activation='relu'))
-    # model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
                   optimizer=Adadelta(),
                   metrics=['accuracy'])
 
-    # # Train the model
+    #
     model.fit(X_train / 255.0, to_categorical(Y_train),
               batch_size=128,
               shuffle=True,
@@ -45,7 +59,6 @@ if __name__ == '__main__':
               validation_data=(X_test / 255.0, to_categorical(Y_test)),
               callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
 
-    # Evaluate the model
     scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
     print(model.summary())
     print('Loss: %.3f' % scores[0])
