f1b0bbd3472cf6e50c07077bef456430ba6e87ff
constantinserediuc <serediuc.constantin@info.uaic.ro>
2019-01-02 19:50:33 +0000

    wip

--- main.py
+++ main.py
@@ -6,33 +6,42 @@ from keras.layers.convolutional import Conv2D
 from keras.optimizers import Adadelta, Adam
 from keras.layers.pooling import MaxPooling2D
 from keras.utils import to_categorical
-
+from keras.layers import BatchNormalization
 if __name__ == '__main__':
     (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
     model = Sequential()
 
     model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.25))
     model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.25))
     model.add(Dense(10, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -40,7 +49,7 @@ if __name__ == '__main__':
                   metrics=['accuracy'])
 
     model.fit(X_train, to_categorical(Y_train),
-              batch_size=128,
+              batch_size=64,
               shuffle=True,
               epochs=250,
               validation_data=(X_test , to_categorical(Y_test)),
